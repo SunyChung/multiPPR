a8d3f9b41fd1110c7c9ec59e3ffe29b71e1ba492
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-05-24 20:41:11 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,26 +1,32 @@
 import numpy as np
-from scipy import sparse
 import pandas as pd
 import pickle
-from scipy.sparse import csr_matrix
+from scipy import sparse
+from scipy.sparse import csr_matrix, isspmatrix_coo
 import os
 import torch
 
 
-def get_bipartite_matrix(data_dir, n_users, n_items):
+def get_bipartite_matrix(data_dir):
+    # "filtered_ratings.csv" 는 아직 index 값으로 mapping 안 된 상태 !!
     df = pd.read_csv(data_dir + 'filtered_ratings.csv')
+    # user, movie indexing 을 위해 dictionary 읽어오고
     with open(data_dir + 'uid_to_uidx.dict', 'rb') as f:
         uid_to_uidx = pickle.load(f)
-    # 근데, movieId 는 train data 에 있는 것만 사용해야 되나 ??
-    # indexing 이 애매하네...
     with open(data_dir + 'mid_to_midx.dict', 'rb') as f:
         mid_to_midx = pickle.load(f)
-    row = df['userId'].apply(lambda x: uid_to_uidx[x])
-    col = df['movieId'].apply(lambda x: mid_to_midx[x])
-    # row = np.array(df['userId']) - 1
-    # col = np.array(df['movieId']) - 1
-    data = np.ones(len(row) * len(col), dtype=int)
-    bi_matrix = csr_matrix((data, (row, col)), shape=(n_users, n_items))
+    # movieId 의 경우, train dataset 에 포함된 Id 만 선택 !!
+    with open(data_dir + 'train_mapped_id', 'rb') as f:
+        train_mapped_id = pickle.load(f)
+
+    row = df['userId'].map(uid_to_uidx)
+    col = df['movieId'].map(mid_to_midx)  # Length: 574548,
+    col = col.loc[col.isin(train_mapped_id)]  # Length: 574514,
+    # .map() or .apply(lambda x : )
+    # row = df['userId'].apply(lambda x: uid_to_uidx[x])
+    # col = df['movieId'].apply(lambda x: mid_to_midx[x])
+    data = np.ones(len(row), dtype=int)
+    bi_matrix = csr_matrix((data, (row, col)), shape=(len(uid_to_uidx), len(train_mapped_id)))
     return bi_matrix
 
 
@@ -30,6 +36,7 @@ def get_movie_matrix(data_dir):
     return movie_matrix
 
 
+# 이것도 index 로 mapping 시켜야 되나 ??
 def get_user_sequences(data_dir):
     df = pd.read_csv(data_dir + 'filtered_ratings.csv')
     df_array = df.to_numpy()
@@ -43,6 +50,7 @@ def get_user_sequences(data_dir):
     # print(np.mean(per_user_item_indices))  # 95.26579340076273
 
 
+# train, validation, test data 는 모두 index 값으로 mapping 된 상태 !
 def load_train_data(csv_file, n_items):
     df = pd.read_csv(csv_file)
     n_users = df['uid'].max() + 1
@@ -69,9 +77,10 @@ def load_tr_te_data(csv_file_tr, csv_file_te, n_items):
 
 
 def load_data(data_dir):
-    with open(os.path.join(data_dir, 'unique_sid'), 'rb') as f:
-        unique_sid = pickle.load(f)
-    n_items = len(unique_sid)  # + 1 ??
+    # 기존 코드들이 사용하던 unique_sid 가 아니라, train_mapped_id 써야 됨 !! 
+    with open(os.path.join(data_dir, 'train_mapped_id'), 'rb') as f:
+        train_mapped_id = pickle.load(f)
+    n_items = len(train_mapped_id)  # + 1 ??
 
     train_data = load_train_data(os.path.join(data_dir, 'train.csv'), n_items)
     vad_data_tr, vad_data_te = load_tr_te_data(os.path.join(data_dir, 'vad_tr.csv'),
@@ -87,17 +96,12 @@ def load_data(data_dir):
     assert n_items == test_data_tr.shape[1]
     assert n_items == test_data_te.shape[1]
     return n_items, train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te
-    return generator
 
 
-def sparse2tensor(data):
-    rows = data.shape[0]
-    cols = data.shape[1]
-    coo_data = data.tocoo()
-    indices = torch.LongTensor([coo_data.rows, coo_data.cols])
-    # 근데, normalize 해야 되나 ?????
-    row_norms_inv = 1 / np.sqrt(data.sum(1))
-    row2val = {i: row_norms_inv[i].item() for i in range(rows)}
-    values = np.array([row2val[r]] for r in coo_data.rows)
-    # torch.Tensor is an alias for the default tensor type (torch.FloatTensor)
-    t = torch.FloatTensor(indices, torch.from_numpy(values).float(), [rows, cols])
+def get_sparse_coord_value_shape(sparse_mat):
+    if not isspmatrix_coo(sparse_mat):
+        sparse_mat = sparse_mat.tocoo()
+    coords = np.vstack((sparse_mat.row, sparse_mat.col)).transpose()
+    values = sparse_mat.data
+    shape = sparse_mat.shape
+    return coords, values, shape
