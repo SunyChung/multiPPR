7a297752aee096271188a3d834d50b8230c4f1ad
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-05-25 21:26:52 +0900

    Update model.py

--- model.py
+++ model.py
@@ -6,8 +6,7 @@ class ContextualizedNN(nn.Module):
     def __init__(self, data_dir, input_dim, hidden_dim, output_dim, top_k):
         super(ContextualizedNN, self).__init__()
 
-        self.movie_mat = get_movie_matrix(data_dir)
-        with open(data_dir + 'per_user_item.dict', 'rb') as f:
+        with open(os.path.join(data_dir, 'per_user_item.dict'), 'rb') as f:
             self.per_user_item_dict = pickle.load(f)
 
         self.com_1 = ItemLinear(input_dim, hidden_dim, output_dim)
@@ -19,8 +18,15 @@ class ContextualizedNN(nn.Module):
         self.interact_linear = nn.Linear(output_dim * 5, 1)
         self.CFs = ContextFeatures(data_dir, top_k)
 
-    def forward(self, user_idx, item_idx):
-        feature_list = self.CFs.neighbor_embeds(target_idx=item_idx)
+    def forward(self, user_idxs, item_idxs):
+        feature_list = self.CFs.neighbor_embeds(target_idx=item_idxs)
+        # ('feature_list len : ', len(feature_list))
+        # print('feature_list[0] : ', feature_list[0])
+        # tensor([1195.,  259., 2857., 1197., 2570.,  592., 2027.,  317., 1269., 2761.,
+        # 1196., 1209., 1264.,  526.,  588.,  607., 2395., 3113.,  295.,  109.])
+        # print('feature_list[0] type : ', type(feature_list))  #  <class 'list'>
+        # print('feature_list[0][0] : ', feature_list[0][0])  # tensor(1195.)
+
         item_rep = torch.cat((self.com_1(feature_list[0], feature_list[1]),
                               self.com_2(feature_list[2], feature_list[3]),
                               self.com_3(feature_list[4], feature_list[5]),
@@ -28,7 +34,7 @@ class ContextualizedNN(nn.Module):
                               self.com_5(feature_list[8], feature_list[9])
         ))
 
-        per_user_items = self.per_user_item_dict[user_idx]
+        per_user_items = self.per_user_item_dict[user_idxs]
         item_list = []
         for i in per_user_items:
             feat = self.CFs.neighbor_embeds(target_idx=i)
@@ -40,8 +46,10 @@ class ContextualizedNN(nn.Module):
             ))
             item_list.append(item)
         user_rep = torch.mean(torch.stack(item_list), dim=0)
-
+        # print('user_rep size : ', user_rep.shape)  # user_rep size :  torch.Size([50])
+        # print('item_rep size : ', item_rep.shape) # item_rep size :  torch.Size([50]) = 10 x 5
         interaction = user_rep * item_rep
+        # print('interaction size : ', interaction.shape)  # interaction size :  torch.Size([50])
         prediction = self.interact_linear(interaction)
         return torch.sigmoid(prediction)
 
