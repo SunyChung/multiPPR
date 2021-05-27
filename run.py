d81753b40bcc843571ddf1e251092093b9075c82
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-05-27 09:30:07 +0900

    Update and rename train.py to run.py

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default='./data/ml-1m/')
 parser.add_argument('--top_k', type=int, default=20)
 parser.add_argument('--damping_factors', type=list, default=[0.30, 0.50, 0.70, 0.85, 0.95])
-parser.add_argument('--batch_size', type=int, default=10, help='the batch size for each epoch')
 parser.add_argument('--learning_rate', type=float, default=0.005, help='initial learning rate')
 parser.add_argument('--epochs', type=int, default=10, help='the number of epochs to train')
+parser.add_argument('--batch_size', type=int, default=100, help='the batch size for each epoch')
 parser.add_argument('--hidden_dim', type=int, default=100)
 parser.add_argument('--output_dim', type=int, default=10)
 
@@ -22,9 +22,9 @@ args = parser.parse_args()
 data_dir = args.data_dir
 top_k = args.top_k
 damping_factors = args.damping_factors
-batch_size = args.batch_size
 learning_rate = args.learning_rate
-epochs = args.epochs
+epochs = 1
+batch_size = args.batch_size
 input_dim = top_k
 hidden_dim = input_dim * 10
 output_dim = args.output_dim
@@ -34,26 +34,37 @@ n_items, train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te = load
 model = ContextualizedNN(data_dir, input_dim, hidden_dim, output_dim, top_k)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+train_coords, train_values, _ = get_sparse_coord_value_shape(train_data)
+# print(train_coords)
+# print(train_coords.shape)  # (480722, 2)
+# print(train_values)
 
-def train(epoch):
+
+def train(epoch, train_coords, train_values):
+    # print(len(train_coords))  # 480722
+    train_n = len(train_coords)
+    idxlist = list(range(train_n))
+
     start = time.time()
     model.train()
     optimizer.zero_grad()
     loss = nn.BCELoss()
     loss_list = []
     # print('train_n : ', train_n)  # 480722
-    for i in range(train_n):
-        # index 는 굳이 Tensor 일 필요없음 !!
-        user_idx = train_coords[i, 0]
-        item_idx = train_coords[i, 1]
-        # print(user_idx)
-        # print(item_idx)
-        prediction = model(user_idx, item_idx)
-        # print('prediction : ', prediction.detach().cpu())
-        # 그냥 다 1로 찍고 있 -_;;;
-        # print(train_values[i])
-        target = torch.Tensor([train_values[i]])
-        train_loss = loss(prediction, target)
+    for batch_num, st_idx in enumerate(range(0, train_n, batch_size)):
+        print('batch num : ', batch_num)
+        end_idx = min(st_idx + batch_size, train_n)
+        user_idxs = train_coords[idxlist[st_idx:end_idx]][:, 0]
+        item_idxs = train_coords[idxlist[st_idx:end_idx]][:, 1]
+        # print(user_idxs.shape)  # (100,)
+        # print(item_idxs.shape)  # (100,)
+        predictions = model(user_idxs, item_idxs)
+        print('predictions : ', predictions)
+        # [tensor([0.5097], grad_fn=<SigmoidBackward>), tensor([0.5104], grad_fn=<SigmoidBackward>),
+        # tensor([0.5083], grad_fn=<SigmoidBackward>), tensor([0.5086], grad_fn=<SigmoidBackward>), ... ]
+        targets = torch.Tensor(train_values[idxlist[st_idx:end_idx]])
+        print('targets : ', targets)
+        train_loss = loss(predictions, targets)
         loss_list.append(train_loss)
         # print(train_loss)
         train_loss.backward()
@@ -64,13 +75,8 @@ def train(epoch):
           'time : {:.4f}s'.format(time.time() - start))
 
 
-train_coords, train_values, _ = get_sparse_coord_value_shape(train_data)
-print(train_coords)
-print(train_coords.shape)
-print(train_values)
-train_n = train_coords.shape[0]
 for epoch in range(epochs):
-    train(epoch)
+    train(epoch, train_coords, train_values)
 
 
 def test():
@@ -88,6 +94,7 @@ def test():
 
 
 test_coords, test_values, _ = get_sparse_coord_value_shape(test_data_tr)
-test_n = test_coords.shape[0]
+test_n = len(test_coords)
 
 loss_list = test()
+print(np.mean(loss_list))
