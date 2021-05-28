f4da24b8615072ea2effa56fe2fc9aaaa69f9652
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-05-28 17:17:42 +0900

    Update model.py

--- model.py
+++ model.py
@@ -14,70 +14,80 @@ class ContextualizedNN(nn.Module):
             self.per_user_item_dict = pickle.load(f)
         self.item_cxt_dict = item_cxt_dict
 
-        self.top_k = top_k
         self.com_1 = ItemLinear(input_dim, hidden_dim, output_dim)
         self.com_2 = ItemLinear(input_dim, hidden_dim, output_dim)
         self.com_3 = ItemLinear(input_dim, hidden_dim, output_dim)
         self.com_4 = ItemLinear(input_dim, hidden_dim, output_dim)
-        self.com_5 = ItemLinear(input_dim, hidden_dim, output_dim)
+        self.com_5 = ItemLinear(input_dim, hidden_dim, output_dim)  # 1 -> 100 -> 10
 
-        self.interact_linear = nn.Linear(output_dim, 1)
+        self.interact_linear = nn.Linear(output_dim * 5, 1)  # 5 factor output -> final output
 
     def forward(self, user_idxs, item_idxs):
-        item_rep_list = []
+        print('NN user_idxs : ', user_idxs)
+        batch_item_rep_list = []
         for i in range(len(item_idxs)):
             item_rep = torch.cat((
-                self.com_1(torch.Tensor(self.item_cxt_dict[item_idxs[i]][0]).unsqueeze(1),
-                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][1]).unsqueeze(1)),
+                self.com_1(torch.Tensor(self.item_cxt_dict[item_idxs[i]][0]),
+                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][1])),
 
-                self.com_2(torch.Tensor(self.item_cxt_dict[item_idxs[i]][2]).unsqueeze(1),
-                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][3]).unsqueeze(1)),
+                self.com_2(torch.Tensor(self.item_cxt_dict[item_idxs[i]][2]),
+                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][3])),
 
-                self.com_3(torch.Tensor(self.item_cxt_dict[item_idxs[i]][4]).unsqueeze(1),
-                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][5]).unsqueeze(1)),
+                self.com_3(torch.Tensor(self.item_cxt_dict[item_idxs[i]][4]),
+                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][5])),
 
-                self.com_4(torch.Tensor(self.item_cxt_dict[item_idxs[i]][6]).unsqueeze(1),
-                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][7]).unsqueeze(1)),
+                self.com_4(torch.Tensor(self.item_cxt_dict[item_idxs[i]][6]),
+                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][7])),
 
-                self.com_5(torch.Tensor(self.item_cxt_dict[item_idxs[i]][8]).unsqueeze(1),
-                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][9]).unsqueeze(1))
+                self.com_5(torch.Tensor(self.item_cxt_dict[item_idxs[i]][8]),
+                           torch.Tensor(self.item_cxt_dict[item_idxs[i]][9]))
                 ))
-            item_rep_list.append(item_rep)
+            # print('item_rep shape : ', item_rep.shape)  # torch.Size([50])
+            batch_item_rep_list.append(item_rep)
+        # 각각의 item index 에 대한 dimension 은 10 (=ouput_dim) x 5 (=factor) = 50
+        # batch_item_rep_list 는 batch size 만큼의 lenght = 100 를 가지는 게 맞음 !
+        # print('batch_item_rep_list length : ', len(batch_item_rep_list))  # 100
 
+        # get item sequence for each user
         batch_user_item_list = [self.per_user_item_dict[i] for i in user_idxs]
-        batch_user_item_rep = []
+        # batch user representation list
+        batch_user_item_rep_list = []
         for i in range(len(batch_user_item_list)):
-            per_user_item_rep = []
+            per_user_item_list = []
             for j in range(len(batch_user_item_list[i])):
                 item_rep = torch.cat((
-                    self.com_1(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][0]).unsqueeze(1),
-                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][1]).unsqueeze(1)),
+                    # item_cxt_dict 자체가 20 개 값을 반환 : 이 값이 20 개의 idx 값
+                    self.com_1(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][0]),
+                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][1])),
 
-                    self.com_2(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][2]).unsqueeze(1),
-                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][3]).unsqueeze(1)),
+                    self.com_2(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][2]),
+                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][3])),
 
-                    self.com_3(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][4]).unsqueeze(1),
-                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][5]).unsqueeze(1)),
+                    self.com_3(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][4]),
+                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][5])),
 
-                    self.com_4(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][6]).unsqueeze(1),
-                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][7]).unsqueeze(1)),
+                    self.com_4(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][6]),
+                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][7])),
 
-                    self.com_5(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][8]).unsqueeze(1),
-                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][9]).unsqueeze(1))
+                    self.com_5(torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][8]),
+                               torch.Tensor(self.item_cxt_dict[batch_user_item_list[i][j]][9]))
                     ))
-                per_user_item_rep.append(item_rep)
-            # 여기서 각 user 별로 item representation 평균내는 거 빠졌음 -_ :;;
-            # 밑에 거는 batch user 임, 개별 user 아님 !!!
-            batch_user_item_rep.append(torch.mean(torch.stack(per_user_item_rep), dim=0))
+                # print('item_rep shape : ', item_rep.shape)  # torch.Size([50])
+                per_user_item_list.append(item_rep)
+            per_user_item_rep = torch.mean(torch.stack(per_user_item_list), dim=0)
+            # print('per_user_item_rep shape : ', per_user_item_rep.shape)  # torch.Size([50])
+            batch_user_item_rep_list.append(per_user_item_rep)
         # 정작 필요한 건 user, item representation 인데,
         # 이게 batch 로 모여 있어서, 중간에 저장할 방법 찾아야 ...
-        # item_rep_list 랑 per_
 
-        interaction_list = [batch_user_item_rep[i] * item_rep_list[i] for i in range(len(batch_user_item_rep))]
-        # print('interaction_list length : ', len(interaction_list))  #  100
-        # 생각해 보니 interaction 은 torch.cat() 안 해도 되겠네 ....
-        batch_prediction = [self.interact_linear(interaction_list[i]) for i in range(len(interaction_list))]
-        print('batch_prediction length : ', len(batch_prediction))
-        result = torch.cat([torch.sigmoid(batch_prediction[i]) for i in range(len(batch_prediction))])
-        print('result shape : ', result.shape)
+        interaction_list = [batch_user_item_rep_list[i] * batch_item_rep_list[i]
+                            for i in range(len(batch_user_item_rep_list))]
+        # print('interaction_list length : ', len(interaction_list))  # 100
+        # print('interaction_list[0] shape : ', interaction_list[0].shape)  # torch.Size([50])
+        batch_prediction = [torch.sigmoid(self.interact_linear(interaction_list[i]))
+                            for i in range(len(interaction_list))]
+        # print('batch_prediction[0] shape : ', batch_prediction[0].shape)  # torch.Size([1])
+        result = torch.stack(batch_prediction).squeeze(1)
+        # print('result shape : ', result.shape)  # torch.Size([100])
+        # 결과적으로는 batch size 만큼의 (=100) prediction 값을 반환해야 함
         return result
