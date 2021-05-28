59da426f5d445b15188df4c48c6cd656e6a70367
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-05-28 17:18:04 +0900

    Update run.py

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ parser.add_argument('--damping_factors', type=list, default=[0.30, 0.50, 0.70, 0
 parser.add_argument('--learning_rate', type=float, default=0.005, help='initial learning rate')
 parser.add_argument('--epochs', type=int, default=10, help='the number of epochs to train')
 parser.add_argument('--batch_size', type=int, default=100, help='the batch size for each epoch')
+parser.add_argument('--input_dim', type=int, default=20, help='should be the top_k node idxs length')
 parser.add_argument('--hidden_dim', type=int, default=100)
 parser.add_argument('--output_dim', type=int, default=10)
 
@@ -25,15 +26,18 @@ damping_factors = args.damping_factors
 learning_rate = args.learning_rate
 epochs = 1
 batch_size = args.batch_size
-input_dim = top_k
+input_dim = args.top_k  # 20
 hidden_dim = input_dim * 10
 output_dim = args.output_dim
+
+with open(os.path.join(data_dir, 'item_cxt_top_20.dict'), 'rb') as f:
+    item_cxt_dict = pickle.load(f)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
 
 n_items, train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te = load_data(data_dir)
-model = ContextualizedNN(data_dir, input_dim, hidden_dim, output_dim, top_k).to(device)
+model = ContextualizedNN(data_dir, item_cxt_dict, input_dim, hidden_dim, output_dim, top_k).to(device)
 print(model)
 pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print('trainable parameters : ', pytorch_total_params)
@@ -43,53 +47,56 @@ train_coords, train_values, _ = get_sparse_coord_value_shape(train_data)
 # print(train_coords)
 # print(train_coords.shape)  # (480722, 2)
 # print(train_values)
+train_n = len(train_coords)
+idxlist = list(range(train_n))
+print('idxlist length : ', len(idxlist))
 
 
 def train(epoch, train_coords, train_values):
     # print(len(train_coords))  # 480722
-    train_n = len(train_coords)
-    idxlist = list(range(train_n))
-
+    print('epoch : ', epoch)
     start = time.time()
     model.train()
     optimizer.zero_grad()
     loss = nn.BCELoss()
     loss_list = []
-    # print('train_n : ', train_n)  # 480722
     # for batch_num, st_idx in enumerate(range(0, train_n, batch_size)):
     for batch_num, st_idx in enumerate(range(0, 110, batch_size)):
         print('batch num : ', batch_num)
-        print('st_idx : ', st_idx)
         end_idx = min(st_idx + batch_size, 110)
         # end_idx = min(st_idx + batch_size, train_n)
-        print('end_idx : ', end_idx)
         user_idxs = train_coords[idxlist[st_idx:end_idx]][:, 0]
+        print('user_idxs : ', user_idxs)
         item_idxs = train_coords[idxlist[st_idx:end_idx]][:, 1]
-        # print(user_idxs.shape)  # (100,)
-        # print(item_idxs.shape)  # (100,)
-        print('item_idxs ', item_idxs)
-        predictions = model(torch.Tensor(user_idxs).to(device), torch.Tensor(item_idxs).to(device))
-        print('predictions : ', predictions)
-        # [tensor([0.5097], grad_fn=<SigmoidBackward>), tensor([0.5104], grad_fn=<SigmoidBackward>),
-        # tensor([0.5083], grad_fn=<SigmoidBackward>), tensor([0.5086], grad_fn=<SigmoidBackward>), ... ]
+
+        predictions = model(user_idxs, item_idxs)
         targets = torch.Tensor(train_values[idxlist[st_idx:end_idx]])
-        print('targets : ', targets)
+        # tensor([1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        #         1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        #         1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        #         1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        #         1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        #         1., 1., 1., 1., 1., 1., 1., 1., 1., 1.])
+        # print('targets shape: ', targets.shape)  # torch.Size([100])
+
         train_loss = loss(predictions, targets)
         loss_list.append(train_loss.detach().numpy())
-        # print(train_loss)
         train_loss.backward()
         optimizer.step()
 
-    print(loss_list)
+    print('loss_list : ', loss_list)
     print('epoch : {:04d}'.format(epoch),
-          'train_loss : {:.4f}'.format(np.mean(loss_list)),
-          'time : {:.4f}s'.format(time.time() - start))
+          '\nmean train_loss : {:.4f}'.format(np.mean(loss_list)),
+          '\ntime : {:.4f}s'.format(time.time() - start))
 
 
+# epoch = 0 에선 잘 돌아가고, 왜 다음 epoch 에서 index 를 못 읽어오나 ??
+# 아, 뒤에 있는 엉청한 코드 땜에 ;;;;;;;;;;;;;;;;
+# 드디어 해결 완료 !! 이제 GPU 가즈아 ~~
 for epoch in range(epochs):
     train(epoch, train_coords, train_values)
 
-
+'''
 def test():
     model.eval()
     loss = nn.BCELoss()
@@ -97,7 +104,7 @@ def test():
     for i in range(test_n):
         user_idx = test_coords[i, 0]
         item_idx = test_coords[i, 1]
-        prediction = model(torch.Tensor(user_idx).to(device), torch.Tensor(item_idx).to(device))
+        prediction = model(user_idx, item_idx)
         target = torch.Tensor([test_values[i]])
         test_loss = loss(prediction, target)
         loss_list.append(test_loss)
@@ -109,3 +116,4 @@ test_n = len(test_coords)
 
 loss_list = test()
 print(np.mean(loss_list))
+'''
