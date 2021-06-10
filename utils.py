e8ef02fe1fd4b476d0ebe940449f9182eaf9f8d8
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-06-10 20:25:24 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from scipy.sparse import csr_matrix, isspmatrix_coo
 from sknetwork.ranking import PageRank
 import os
 import time
+import bottleneck as bn
 
 
 def get_bipartite_matrix(data_dir):
@@ -157,6 +158,32 @@ class MultiPPR(object):
             multi_score.append(np.array(sorted_scores))
             indices.append(np.array(idx))
         return np.array(multi_score), np.array(indices)
+
+
+def NDCG(X_pred, heldout_batch, k=100):
+    batch_users = X_pred.shape[0]
+    idx_topk_part = bn.argpartition(-X_pred, k, axis=1)
+    topk_part = X_pred[np.arange(batch_users)[:, np.newaxis], idx_topk_part[:, :k]]
+    idx_part = np.argsort(-topk_part, axis=1)
+    # topk predicted score
+    idx_topk = idx_topk_part[np.arange(batch_users)[:, np.newaxis], idx_part]
+    # build the discount template
+    tp = 1. / np.log2(np.arange(2, k + 2))
+    DCG = (heldout_batch[np.arange(batch_users)[:, np.newaxis], idx_topk].toarray() * tp).sum(axis=1)
+    IDCG = np.array([(tp[:min(n, k)]).sum() for n in heldout_batch.getnnz(axis=1)])
+    return DCG / IDCG
+
+
+def RECALL(X_pred, heldout_batch, k=100):
+    batch_users = X_pred.shape[0]
+    idx = bn.argpartition(-X_pred, k, axis=1)
+
+    X_pred_binary = np.zeros_like(X_pred, dtype=bool)
+    X_pred_binary[np.arange(batch_users)[:, np.newaxis], idx[:, :k]] = True
+    X_true_binary = (heldout_batch > 0).toarray()
+    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(np.float32)
+    recall = tmp / np.minimum(k, X_true_binary.sum(axis=1))
+    return recall
 
 
 if __name__ == '__main__':
