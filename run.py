ef6fccf50590fa435d91f0ca34ca35ed39a0028d
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-06-12 01:59:37 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import time
 import argparse
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -17,6 +19,7 @@ parser.add_argument('--batch_size', type=int, default=500)
 parser.add_argument('--epochs', type=int, default=20)
 parser.add_argument('--multi_factor', type=int, default=5)
 parser.add_argument('--top_k', type=int, default=20)
+parser.add_argument('--emb_dim', type=int, default=100)
 parser.add_argument('--input_dim', type=int, default=20, help='top_k x multi_factor')
 parser.add_argument('--hidden_dim', type=int, default=100)
 parser.add_argument('--output_dim', type=int, default=10)
@@ -30,39 +33,45 @@ batch_size = args.batch_size
 epochs = args.epochs
 multi_factor = args.multi_factor
 top_k = args.top_k
-input_dim = top_k * multi_factor
-hidden_dim = input_dim * 5
-output_dim = multi_factor * 10
-final_dim = args.final_dim
-
-# item context dictionary making
-with open(os.path.join(data_dir, 'per_item_idx.dict'), 'rb') as f:
-    item_idx_dict = pickle.load(f)
-with open(os.path.join(data_dir, 'per_item_ppr.dict'), 'rb') as f:
-    item_ppr_dict = pickle.load(f)
-pf = PPRfeatures(data_dir, top_k, item_idx_dict, item_ppr_dict)
-item_idx_emb = pf.idx_embeds()
-item_scr_emb = pf.score_embeds()
-del item_idx_dict
-del item_ppr_dict
-
-# user context dictionary making
-with open(os.path.join(data_dir, 'per_user_ppr.dict'), 'rb') as f:
-    user_idx_dict = pickle.load(f)
-with open(os.path.join(data_dir, 'per_user_idx.dict'), 'rb') as f:
-    user_ppr_dict = pickle.load(f)
-pf = PPRfeatures(data_dir, top_k, user_idx_dict, user_ppr_dict)
-user_idx_emb = pf.idx_embeds()
-user_scr_emb = pf.score_embeds()
-del user_idx_dict
-del user_ppr_dict
+emb_dim = args.emb_dim
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
 
+
+# item context tensor
+with open(os.path.join(data_dir, 'per_item_idx.dict'), 'rb') as f:
+    item_idx_dict = pickle.load(f)
+with open(os.path.join(data_dir, 'per_item_ppr.dict'), 'rb') as f:
+    item_ppr_dict = pickle.load(f)
+
+pf = PPRfeatures(data_dir, top_k, item_idx_dict, item_ppr_dict)
+item_idx_tensor = pf.idx_tensor()
+item_scr_tensor = pf.score_tensor()
+del item_idx_dict
+del item_ppr_dict
+
+# user context tensor
+with open(os.path.join(data_dir, 'per_user_idx.dict'), 'rb') as f:
+    user_idx_dict = pickle.load(f)
+with open(os.path.join(data_dir, 'per_user_ppr.dict'), 'rb') as f:
+    user_ppr_dict = pickle.load(f)
+
+pf = PPRfeatures(data_dir, top_k, user_idx_dict, user_ppr_dict)
+user_idx_tensor = pf.idx_tensor()
+user_scr_tensor = pf.score_tensor()
+del user_idx_dict
+del user_ppr_dict
+
+# for user embedding size
+with open(os.path.join(data_dir, 'unique_uidx'), 'rb') as f:
+    unique_uidx = pickle.load(f)
 n_items, train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te = load_data(data_dir)
-model = ContextualizedNN(item_idx_emb, item_scr_emb, user_idx_emb, user_scr_emb,
-                         multi_factor, input_dim, hidden_dim, output_dim, final_dim).to(device)
+item_embedding = nn.Embedding(n_items, emb_dim)
+user_embedding = nn.Embedding(len(unique_uidx), emb_dim)
+
+model = ContextualizedNN(item_idx_tensor, item_scr_tensor, user_idx_tensor, user_scr_tensor,
+                         item_embedding, user_embedding).to(device)
 print(model)
 pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print('trainable parameters : ', pytorch_total_params)
@@ -71,20 +80,10 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 train_coords, train_values, _ = get_sparse_coord_value_shape(train_data)
 vad_tr_coords, vad_trv_values, _ = get_sparse_coord_value_shape(vad_data_tr)
 vad_te_coords, vad_te_values, _ = get_sparse_coord_value_shape(vad_data_te)
-# print(train_coords)  # user, item index lists
 # print(train_coords.shape)  # (480722, 2)
-# print(train_values)
+item_rank = ItemRanking(user_embedding, item_embedding, top_k=100)
 
 
-# RecVAE, VAE_cf 랑 학습 방식에 차이가 있음 !
-# VAE-based 방식은 user 별로 전체 item 에 대한 interaction probability 를 학습하는 구조고,
-# 내 모델은 주어진 user-item pair index 를 사용해, 각각의 representaiton 을 학습함
-# 일단, train_data 에 포함된 item 에 대해서만, 학습이 이루어지기 때문에,
-# train_data 로 학습한 item representation 은 validation, test 에서도 사용 가능
-# 대신, user representation 은 PPR 을 사용해, 유추하는 방식으로, (이것도 될 것 같긴 함...)
-# 근데, 이 방식에서는 validation 에서 validation train 을 validation test 에 적용 가능한가?
-# train, test index 부터 다른 거 같은데 -_
-# 그냥 validation test 로만 성능 평가해도 될 듯 ....
 def train(epoch, train_coords, train_values, vad_te_coords, vad_te_values):
     print('epoch : ', epoch)
     start = time.time()
@@ -106,31 +105,34 @@ def train(epoch, train_coords, train_values, vad_te_coords, vad_te_values):
         train_loss.backward()
         optimizer.step()
 
-    vad_n = len(vad_te_coords)
-    vad_idxlist = list(range(vad_n))
-    ndcg_list = []
-    for batch_num, st_idx in enumerate(range(0, vad_n, batch_size)):
-        end_idx = min(st_idx + batch_size, vad_n)
-        user_idxs = vad_te_coords[vad_idxlist[st_idx:end_idx]][:, 0]
-        item_idxs = vad_te_coords[vad_idxlist[st_idx:end_idx]][:, 1]
-
-        predictions = model(user_idxs, item_idxs)
-        # 근데, 여기서 문제는 .detach().numpy() or .to('cpu') 이런 게 아니고,
-        # prediction 결과값으로 NDCG 계산이 안 된다는 거 아닌가 ;;
-        # 여기서의 prediction 은 user 랑 item prbiability 인데,
-        # NDCG 는 item ranking 이 필요함...
-        # 그냥 코드 돌리지 말고, 먼저, 각 user 별로 item ranking 뽑을 방법 찾기 !
-        ndcg_list.append(NDCG(predictions.detach().numpy(), vad_te_values[vad_idxlist[st_idx:end_idx]]))
-    ndcg_list = np.concatenate(ndcg_list)
-    ndcg = ndcg_list.mean()
+    # for validation only validation user idx is needed
+    # evaluation is done by comparing the actual item sequence with prediction using embedding values
+    ndcg_cos_list = []
+    ndcg_cdist_list = []
+    # 근데 여기서는 pariwise 로 coordinate 를 만들어서, user_idx 중복이 많음 ...
+    # validation, test 에서는 batch 하지 말고, 그냥 user id 추출해서 테스트 하는 게 나을 듯
+    vad_row, vad_col = vad_data_te.nonzero()
+    uniq_vad_users = np.unique(vad_row)
+    for i in enumerate(range(uniq_vad_users)):
+        cos_item_pred, cdist_item_pred = item_rank.calculate(uniq_vad_users[i])
+        user_items = [j for j in range(len(vad_data_te.toarray()[uniq_vad_users[i]]))
+                      if vad_data_te.toarray()[uniq_vad_users[i]][j] == 1]
+        # 예측값 : cos_item_pred, cdist_item_pred , 각 사용자별 item 정답 : user_items
+        ndcg_cos_list.append(ndcg(user_items, cos_item_pred, len(user_items)))
+        ndcg_cdist_list.append(ndcg(user_items, cdist_item_pred, len(user_items)))
+    ndcg_cos_list = np.concatenate(ndcg_cos_list)
+    ndcg_cdist_list = np.concatenate((ndcg_cdist_list))
+    ndcg_cos = ndcg_cos_list.mean()
+    ndcg_cdist = ndcg_cdist_list.mean()
     print('epoch : {:04d}'.format(epoch),
           '\ntime : {:.4f}s'.format(time.time() - start))
-    return ndcg
+    return ndcg_cos, ndcg_cdist
 
 
 for epoch in range(epochs):
-    ndcg = train(epoch, train_coords, train_values, vad_te_coords, vad_te_values)
-    print('NDCG : ', ndcg)
+    ndcg_cos, ndcg_cdist = train(epoch, train_coords, train_values, vad_te_coords, vad_te_values)
+    print('cosine-based NDCG : ', ndcg_cos)
+    print('distance-based NDCG : ', ndcg_cdist)
 
 
 # 근데, 이렇게 되면, 예측이 너무 뻔해지는데;;
