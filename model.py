25017b05cf1c5169ea573a45498f18074e43b6f4
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-06-15 17:36:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -35,10 +35,10 @@ class ContextualizedNN(nn.Module):
 
         interaction = scored_user_emb * scored_item_emb
         result = torch.sigmoid(self.inter_lin(interaction))
-        print('result : ', result)
-        print('result shape : ', result.shape)
+        # print('result shape : ', result.shape)
         # for batch training : torch.Size([500, 100, 1])
         # for one id evaluation : torch.Size([100, 1])
+        print('final result shape : ', torch.mean(result, dim=-2).squeeze().shape())
         return torch.mean(result, dim=-2).squeeze()
 
 
