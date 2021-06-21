cf243ef4825802c1d213e5f7806b51c7c3207a04
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-06-21 15:22:48 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,9 @@
 import numpy as np
 import pandas as pd
 import pickle
-
 from scipy import sparse
 from scipy.sparse import csr_matrix, isspmatrix_coo
-from sknetwork.ranking import PageRank
 import os
-import time
-import bottleneck as bn
 
 
 def get_bipartite_matrix(data_dir):
@@ -123,103 +119,3 @@ def get_sparse_coord_value_shape(sparse_mat):
     values = sparse_mat.data
     shape = sparse_mat.shape
     return coords, values, shape
-
-
-class MultiPPR(object):
-    def __init__(self, damping_factors, matrix):
-        super(MultiPPR, self).__init__()
-        self.damping_factors = damping_factors
-        self.mat = matrix
-
-    def multi_contexts(self, target_idx):
-        base_zeros = np.zeros(self.mat.shape[0] - 1)
-        seed = np.insert(base_zeros, target_idx, 1)
-        multi_score = []
-        indices = []
-        for i in range(len(self.damping_factors)):
-            pagerank = PageRank(damping_factor=self.damping_factors[i])
-            ppr = pagerank.fit_transform(self.mat.toarray(), seeds=seed)
-            # don't need to exclude the first element from the start
-            # can filtering the idx afterwards, if necessary
-            idx = np.argsort(ppr)[::-1]
-            sorted_scores = ppr[idx]
-            multi_score.append(np.array(sorted_scores))
-            indices.append(np.array(idx))
-        return np.array(multi_score), np.array(indices)
-
-
-def NDCG(pred_rel, item_idxs, k):
-    print('pred_rel : ', pred_rel)
-    # 이거 다 1 만 찍고 있는데, 왜 argsort() 는 작은 값을 정렬하고 있는 건지 ?!
-    topk_idx = np.argsort(pred_rel)[::-1][:k]
-    topk_pred = pred_rel[topk_idx]
-    # print('topk_pred : ', topk_pred)
-    discount = 1. / np.log2(np.arange(2, k + 2))
-    dcg = np.array([topk_pred / discount]).sum()
-    idcg = np.array([discount[:min(len(item_idxs), k)]]).sum()
-    # print('len(item_idx) : ', len(item_idxs))
-    # print('idcg : ', idcg)
-    return dcg / idcg
-
-
-def RECALL(pred_rel, item_idx, k):
-    topk_idx = np.argsort(pred_rel)[::-1][:k]
-    pred_binary = np.zeros_like(pred_rel, dtype=bool)
-    pred_binary[topk_idx] = True
-    true_binary = np.zeros_like(pred_rel, dtype=bool)
-    true_binary[item_idx] = True
-    tmp = (np.logical_and(pred_binary, true_binary).sum()).astype(np.float32)
-    recall = tmp / np.minimum(k, true_binary.sum())
-    return recall
-
-
-if __name__ == '__main__':
-    # data_dir = './data/ml-1m/'
-    data_dir = './data/ml-20m/'
-    damping_factors = [0.30, 0.50, 0.70, 0.85, 0.95]
-
-    movie_mat = get_movie_matrix(data_dir)
-    print('movie_mat shape : ', movie_mat.shape)
-    multi_ppr = MultiPPR(damping_factors, movie_mat)
-    start = time.time()
-    per_item_ppr_dict = {}
-    per_item_idx_dict = {}
-    for i in range(movie_mat.shape[0]):
-        scores, indices = multi_ppr.multi_contexts(i)
-        per_item_ppr_dict[i] = scores
-        per_item_idx_dict[i] = indices
-        if i % 1000 == 0:
-            print('%d nodes processed!' % i)
-            print('upto now %f seconds passed' % (time.time() - start))
-    end = time.time()
-    print('multi-ppr processing takes : ', end - start)
-    with open(os.path.join(data_dir, 'per_item_ppr.dict'), 'wb') as f:
-        pickle.dump(per_item_ppr_dict, f)
-    with open(os.path.join(data_dir, 'per_item_idx.dict'), 'wb') as f:
-        pickle.dump(per_item_idx_dict, f)
-
-
-    '''
-    user_mat = get_user_matrix(data_dir)
-    print('user_mat shape : ', user_mat.shape)
-    multi_ppr = MultiPPR(damping_factors, user_mat)
-
-    start = time.time()
-    # the default dictionary type is sufficient 'cause the return values already has multi array values !!!
-    per_user_ppr_dict = {}
-    per_user_idx_dict = {}
-    for i in range(user_mat.shape[0]):
-        scores, indices = multi_ppr.multi_contexts(i)
-        per_user_ppr_dict[i] = scores
-        per_user_idx_dict[i] = indices
-        if i % 300 == 0:
-            print('%d nodes processed!' % i)
-            print('upto now %f seconds passed' % (time.time() - start))
-    end = time.time()
-    print('multi-ppr processing takes : ', end - start)
-
-    with open(os.path.join(data_dir, 'per_user_ppr.dict'), 'wb') as f:
-        pickle.dump(per_user_ppr_dict, f)
-    with open(os.path.join(data_dir, 'per_user_idx.dict'), 'wb') as f:
-        pickle.dump(per_user_idx_dict, f)
-    '''
