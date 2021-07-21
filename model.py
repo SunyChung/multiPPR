6d6fb7db773941f1552677e28fe27679613821f2
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-07-21 13:33:05 +0900

    Update model.py

--- model.py
+++ model.py
@@ -37,35 +37,40 @@ class ContextualizedNN(nn.Module):
 
         self.cat_dim = self.item_emb.embedding_dim * 2
 
-        # self.inter_lin_1 = InterLin_1(input_dim=self.cat_dim, output_dim=1)
+        # self.inter_layer = InterLin_1(input_dim=self.cat_dim, output_dim=1)
 
-        # self.inter_lin_3 = InterLin_3(input_dim=self.cat_dim,
+        # self.inter_layer = InterLin_3(input_dim=self.cat_dim,
         #                          hidden1=self.cat_dim // 2,
         #                          hidden2=self.cat_dim // 4,
         #                          output_dim=1)
 
 
-        # self.inter_lin_5 = InterLin_5(input_dim=self.cat_dim,
+        # self.inter_layer = InterLin_5(input_dim=self.cat_dim,
         #                               hidden1=self.cat_dim // 2,
         #                               hidden2=self.cat_dim // 4,
         #                               output_dim=1)
 
-        # self.inter_lin_5_norm = InterLin_5_norm(input_dim=self.cat_dim,
-        #                                       hidden1=self.cat_dim // 2,
-        #                                       hidden2=self.cat_dim // 4,
-        #                                       output_dim=1)
+        # self.inter_layer = InterLin_5_norm(input_dim=self.cat_dim,
+        #                                    hidden1=self.cat_dim // 2,
+        #                                    hidden2=self.cat_dim // 4,
+        #                                    output_dim=1)
 
-        # self.inter_lin_7 = InterLin_7(input_dim=self.cat_dim,
+        self.inter_layer = InterLin_5_no_relu(input_dim=self.cat_dim,
+                                           hidden1=self.cat_dim // 2,
+                                           hidden2=self.cat_dim // 4,
+                                           output_dim=1)
+
+        # self.inter_layer = InterLin_7(input_dim=self.cat_dim,
         #                               hidden1=self.cat_dim // 2,
         #                               hidden2=self.cat_dim // 4,
         #                               hidden3=self.cat_dim // 8,
         #                               output_dim=1)
 
-        self.inter_lin_8 = InterLin_8(input_dim=self.cat_dim,
-                                      hidden1=self.cat_dim // 2,
-                                      hidden2=self.cat_dim // 4,
-                                      hidden3=self.cat_dim // 8,
-                                      output_dim=1)
+        # self.inter_layer = InterLin_8(input_dim=self.cat_dim,
+        #                               hidden1=self.cat_dim // 2,
+        #                               hidden2=self.cat_dim // 4,
+        #                               hidden3=self.cat_dim // 8,
+        #                               output_dim=1)
 
 
     def forward(self, user_idxs, item_idxs):
@@ -89,13 +94,9 @@ class ContextualizedNN(nn.Module):
 
         interaction_cat = torch.cat((scored_user_emb, scored_item_emb), 2)
         # with BCELoss()
-        # result = torch.sigmoid(self.inter_lin_1(interaction_cat))
-        # result = torch.sigmoid(self.inter_lin_3(interaction_cat))
+        # result = torch.sigmoid(self.inter_layer(interaction_cat))
         # with BCEWithLogitsLoss()
-        # result = self.inter_lin_1(interaction_cat)
-        # result = self.inter_lin_5(interaction_cat)
-        # result = self.inter_lin_7(interaction_cat)
-        result = self.inter_lin_8(interaction_cat)
+        result = self.inter_layer(interaction_cat)
         return torch.mean(result, dim=-2).squeeze()
 
 
@@ -145,27 +146,45 @@ class InterLin_5(nn.Module):
         return x
 
 
+# epoch 20 에서, 중간에 loss 가 1 까지 올라갔으나, test 에서 0.24, 0.28 로 엄청 나쁘진 않음 ...
+# epoch 을 늘려서 더 해 봐야 하나 ?!
+# epoch 50 에서는 심지어 loss 가 3 이 넘어도, 0.31, 0.32 가 나옴 -_
+# relu() 가 없어서 그런지 epoch 을 반복하면 loss 가 22 까지 올라감 ;; evaluation 값은 좋음 ...
+# 뭔가 판단할 기준이 제대로 없는 ... BPR loss 함께 써야 될 듯 ??
+class InterLin_5_no_relu(nn.Module):
+    def __init__(self, input_dim, hidden1, hidden2, output_dim):
+        super(InterLin_5_no_relu, self).__init__()
+
+        self.ln1 = nn.Linear(input_dim, hidden1)
+        self.ln2 = nn.Linear(hidden1, hidden1)
+        self.ln3 = nn.Linear(hidden1, hidden2)
+        self.ln4 = nn.Linear(hidden2, hidden2)
+        self.ln5 = nn.Linear(hidden2, output_dim)
+
+    def forward(self, x):
+        x = self.ln1(x)
+        x = self.ln2(x)
+        x = self.ln3(x)
+        x = self.ln4(x)
+        x = self.ln5(x)
+        return x
+
+
 class InterLin_5_norm(nn.Module):
     def __init__(self, input_dim, hidden1, hidden2, output_dim):
         super(InterLin_5_norm, self).__init__()
 
         self.fc1 = nn.Linear(input_dim, hidden1)
         self.ln1 = nn.LayerNorm(hidden1, eps=1e-4)
-        self.fc2 = nn.Linear(hidden1, hidden1)
-        self.ln2 = nn.LayerNorm(hidden1, eps=1e-4)
-        self.fc3 = nn.Linear(hidden1, hidden2)
-        self.ln3 = nn.LayerNorm(hidden2, eps=1e-4)
-        self.fc4 = nn.Linear(hidden2, hidden2)
-        self.ln4 = nn.LayerNorm(hidden2, eps=1e-4)
-        self.fc5 = nn.Linear(hidden2, output_dim)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.ln2 = nn.LayerNorm(hidden2, eps=1e-4)
+        self.fc3 = nn.Linear(hidden2, output_dim)
 
     def forward(self, x):
-        h1 = self.ln1(F.relu(self.fc1(x)))
+        h1 = self.ln1(F.relu(self.fc1(x)))  # 32
         h2 = self.ln2(F.relu(self.fc2(h1) + h1))
         h3 = self.ln3(F.relu(self.fc3(h2) + h1 + h2))
-        h4 = self.ln4(F.relu(self.fc4(h3) + h1 + h2 + h3))
-        h5 = self.fc5(h4)
-        return h5
+        return h3
 
 
 class InterLin_7(nn.Module):
