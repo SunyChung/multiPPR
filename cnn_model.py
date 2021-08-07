6b27a36dd9d2643f24e4bbc35f5e09673d093da0
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-08-07 13:55:52 +0900

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -8,16 +8,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class FeatureEmb(object):
+class FeatureEmb(nn.Module):
     def __init__(self, item_idx_tensor, item_scr_tensor,
                  user_idx_tensor, user_scr_tensor,
                  item_embedding, user_embedding,
                  n_items, top_k, multi_factor, emb_dim):
         super(FeatureEmb, self).__init__()
 
-        self.item_idx_tensor = item_idx_tensor  # torch.Size([3515, multi_factor x # of neighbors])
+        self.item_idx_tensor = item_idx_tensor
+        # torch.Size([3515, multi_factor x # of neighbors])
         self.item_scr_tensor = item_scr_tensor
-        self.user_idx_tensor = user_idx_tensor  # torch.Size([6034, multi_factor x # of neighbors])
+        self.user_idx_tensor = user_idx_tensor
+        # torch.Size([6034, multi_factor x # of neighbors])
         self.user_scr_tensor = user_scr_tensor
         self.n_items = n_items
         self.top_k = top_k
@@ -51,33 +53,33 @@ class FeatureEmb(object):
         self.c_net = ConvNet(chan_1=64, chan_2=32)
 
     def forward(self, user_idxs, item_idxs):
-        # print('user_idxs shape : ', user_idxs.shape)  # (3516,)
+        # print('user_idxs shape : ', user_idxs.shape)  # (3516,) -> (3515,) -_;;
         user_neighs = self.user_idx_tensor[user_idxs]
-        print('\nuser_neighs shape : ', user_neighs.shape)
-        # torch.Size([3516(=batch_size=# of items), multi_factor x top_k])
+        # print('\nuser_neighs shape : ', user_neighs.shape)
+        # torch.Size([3515 (=batch_size=# of items), multi_factor x top_k])
         neigh_emb = self.user_emb(user_neighs)
-        print('neigh_emb shape : ', neigh_emb.shape)
-        # torch.Size([3516(=batch_size), multi_factor x top_k, embedding_dim])
+        # print('neigh_emb shape : ', neigh_emb.shape)
+        # torch.Size([3515 (=batch_size), multi_factor x top_k, embedding_dim])
         neigh_score = self.user_scr_tensor[user_neighs]
-        print('neigh_score shape : ', neigh_score.shape)
-        # torch.Size([3516(=batch_size), multi_factor x top_k, multi_factor x top_k])
+        # print('neigh_score shape : ', neigh_score.shape)
+        # torch.Size([3515 (=batch_size), multi_factor x top_k, multi_factor x top_k])
         scored_user_emb = torch.bmm(neigh_score, neigh_emb)
-        print('scored_user_emb shape : ', scored_user_emb.shape)
-        # torch.Size([3516(=batch_size), multi_factor x top_k, embedding_dim])
+        # print('scored_user_emb shape : ', scored_user_emb.shape)
+        # torch.Size([3515 (=batch_size), multi_factor x top_k, embedding_dim])
         summed_user = {}
         for i in range(self.multi_factor):
             # print('before sum shape : ', scored_user_emb[:, self.top_k * i: self.top_k * (i+1), :].shape)
-            # torch.Size([3516, 20, 64])
+            # torch.Size([3515, 20, 64])
             summed_user[i] = torch.sum(scored_user_emb[:, self.top_k * i: self.top_k * (i+1), :], dim=1)
-            # print('summed_user shape: ', summed_user[i].shape)  # torch.Size([3516, 64])
+            # print('summed_user shape: ', summed_user[i].shape)  # torch.Size([3515, 64])
             # print('summed_user : ', summed_user[i])
 
         concat_user_emb = torch.cat(list(summed_user.values()), dim=1)
         stacked_user_emb = torch.stack(list(summed_user.values()), dim=1)
         # concat keep the given dimension
-        print('\ncon_user_emb shape : ', concat_user_emb.shape)  # torch.Size([3516, 320])
+        # print('\ncon_user_emb shape : ', concat_user_emb.shape)  # torch.Size([3515, 320])
         # stack makes a new dimension
-        print('stacked_user_emb shape : ', stacked_user_emb.shape)  # torch.Size([3516, 5, 64])
+        # print('stacked_user_emb shape : ', stacked_user_emb.shape)  # torch.Size([3515, 5, 64])
 
         item_neighs = self.item_idx_tensor[item_idxs]
         item_neigh_emb = self.item_emb(item_neighs)
@@ -86,30 +88,37 @@ class FeatureEmb(object):
         summed_item = {}
         for i in range(self.multi_factor):
             summed_item[i] = torch.sum(scored_item_emb[:, self.top_k * i: self.top_k * (i+1), :], dim=1)
-        concat_item_emb = torch.cat(list(summed_item.values()), dim=1)  # torch.Size([17580, 64])
-        stacked_item_emb = torch.stack(list(summed_item.values()), dim=1)  # torch.Size([3516, 5, 64])
+        concat_item_emb = torch.cat(list(summed_item.values()), dim=1)
+        stacked_item_emb = torch.stack(list(summed_item.values()), dim=1)  # torch.Size([3515, 5, 64])
 
         concat_user_item = torch.mm(concat_user_emb, concat_item_emb.T)
-        # print('concat_user_item shape : ', concat_user_item.shape)  # torch.Size([17580, 17580])
+        # print('concat_user_item shape : ', concat_user_item.shape)
         stacked_user_item = torch.bmm(stacked_user_emb, stacked_item_emb.permute(0, 2, 1))
-        # print('stacked_user_item shape : ', stacked_user_item.shape)  # torch.Size([3516, 5, 5])
+        # print('stacked_user_item shape : ', stacked_user_item.shape)  # torch.Size([3515, 5, 5])
         # print('len(stacked_user_item) : ', len(stacked_user_item))
 
         results = []
         for i in range(len(stacked_user_item)):
-            # print('input shape : ',
-            #       stacked_user_item[i, :, :].unsqueeze_(0).unsqueeze_(0).shape)
+            print('stacked_user_item : ', stacked_user_item[i].shape)  # [5, 5]
+            print('input shape : ', (stacked_user_item[i].reshape(1, 1, -1, -1)).shape)
             #  torch.Size([1, 1, 5, 5])
             result = torch.sigmoid(
-                self.c_net(stacked_user_item[i, :, :].unsqueeze_(0).unsqueeze_(0)))
+                self.c_net(stacked_user_item[i].view(1, 1, -1, -1)))
+                # self.c_net(stacked_user_item[i, :, :].unsqueeze_(0).unsqueeze_(0)))
             # print('result shape : ', result.shape)
             # torch.Size([1])
+            # print('result : ', result)
+            # tensor([0.8552], device='cuda:0', grad_fn=<SigmoidBackward>)
             results.append(result)
-        return results
-
+        # print('results : ', results)
+        # ... , grad_fn=<SigmoidBackward>), tensor([0.9992], grad_fn=<SigmoidBackward>)]
+        # print('results shape : ', len(results))  # 3515
+        # return torch.Tensor(results)
+        # return results  # 리스트로 반환하면, gradient tag 가 사라져서 loss 연산 불가 !! 주의 !!
+        return torch.cat(results)
 
 class ConvNet(nn.Module):
-    def __init__(self, chan_1=64, chan_2=32):
+    def __init__(self, chan_1, chan_2):
         super().__init__()
         self.conv1 = nn.Conv2d(1, chan_1, (1, 5))
         self.conv2 = nn.Conv2d(chan_1, chan_2, (5, 1))
@@ -161,11 +170,12 @@ if __name__ == '__main__':
         unique_uidx = pickle.load(f)
     item_embedding = nn.Embedding(n_items, emb_dim)
     user_embedding = nn.Embedding(len(unique_uidx), emb_dim)
+
     femb = FeatureEmb(item_idx_tensor, item_scr_tensor,
                       user_idx_tensor, user_scr_tensor,
                       item_embedding, user_embedding,
                       n_items, top_k, multi_factor, emb_dim)
-
     results \
-        = femb.forward(np.random.randint(0, 6000, 3516), np.random.randint(0, 3000, 3516))
-    print(results)
+        = femb(np.random.randint(0, 6000, 3515), np.random.randint(0, 3000, 3515))
+    # print('results length : ', len(results))
+    # print('results : ', results)
