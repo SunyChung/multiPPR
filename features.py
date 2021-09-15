28f89cce6a72433f3b895c71059897eb846149c8
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-09-15 15:17:58 +0900

    Update features.py

--- features.py
+++ features.py
@@ -1,139 +1,133 @@
-import numpy as np
-import pandas as pd
-import pickle
-from scipy.sparse import csr_matrix
 import os
+import pickle
 import time
-from collections import Counter
-from collections import defaultdict
+import numpy as np
+import torch
+from sknetwork.ranking import PageRank
+
+from utils import get_movie_matrix, get_user_matrix
 
 
-def get_bipartite_matrix(data_dir):
-    df = pd.read_csv(os.path.join(data_dir, 'filtered_pd.csv'))
-    # load filtered & idx mapped dataframe !
-    col = df['movieId']
-    row = df['userId']
-    data = np.ones(len(row), dtype=int)
-    print('bipartite mat : max(row) = ', max(row))
-    print('bipartite mat : max(col) = ', max(col))
-    print('unique # of row : ', len(pd.unique(row)))
-    print('unique # of col : ', len(pd.unique(col)))
-    bi_matrix = csr_matrix((data, (row, col)), shape=(max(row)+1, max(col)+1))
-    return bi_matrix
+class PPRfeatures(object):
+    def __init__(self, top_k, idx_dict, ppr_dict):
+        self.idx_values = np.array(list(idx_dict.values()))[:, :, :top_k]
+        # print('idx_values shape : ', self.idx_values.shape)  # (3515, 5, 50)
+        self.ppr_scores = np.array(list(ppr_dict.values()))[:, :, :top_k]
+
+    def reshaped_idx_tensor(self):
+        reshaped = self.idx_values.reshape(self.idx_values.shape[0], -1)
+        idx_tensor = torch.LongTensor(reshaped)
+        # print('idx_tensor shape : ', idx_tensor.shape)  # torch.Size([3515, 250])
+        # idx_tensor = torch.LongTensor(self.idx_values)
+        # [# items/users, multi-factors, top_k]
+        return idx_tensor
+
+    def reshaped_scr_tensor(self):
+        reshaped = self.ppr_scores.reshape(self.ppr_scores.shape[0], -1)
+        scr_tensor = torch.FloatTensor(reshaped)
+        # print('scr_tensor shape : ', scr_tensor.shape)  # torch.Size([3515, 250])
+        # scr_tensor = torch.LongTensor(self.ppr_scores)
+        # [# items/users, multi-factors, top_k]
+        return scr_tensor
 
 
-def get_movie_matrix(data_dir):
-    bi_matrix = get_bipartite_matrix(data_dir)
-    # weighted
-    movie_matrix = bi_matrix.transpose() * bi_matrix
-    '''
-    row, col = movie_matrix.nonzero()
-    movie_adj = csr_matrix((np.ones_like(row), (row, col)),
-                           shape=(max(row)+1, max(col)+1))
-    # 굳이 adj 로 바꿀 필요 없음 ... ;
-    return movie_adj
-    '''
-    return movie_matrix
+'''
+if __name__ == '__main__':
+    data_dir = './data/ml-1m'
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    top_k = 20
+
+    # item context tensor
+    with open(os.path.join(data_dir, 'per_item_idx.dict'), 'rb') as f:
+        item_idx_dict = pickle.load(f)
+    with open(os.path.join(data_dir, 'per_item_ppr.dict'), 'rb') as f:
+        item_ppr_dict = pickle.load(f)
+    pf = PPRfeatures(top_k, item_idx_dict, item_ppr_dict)
+    item_idx_tensor = pf.idx_tensor()
+    item_scr_tensor = pf.scr_tensor()
+    del item_idx_dict
+    del item_ppr_dict
+
+    # user context tensor
+    with open(os.path.join(data_dir, 'per_user_idx.dict'), 'rb') as f:
+        user_idx_dict = pickle.load(f)
+    with open(os.path.join(data_dir, 'per_user_ppr.dict'), 'rb') as f:
+        user_ppr_dict = pickle.load(f)
+    pf = PPRfeatures(top_k, user_idx_dict, user_ppr_dict)
+    user_idx_tensor = pf.idx_tensor()
+    user_scr_tensor = pf.scr_tensor()
+    del user_idx_dict
+    del user_ppr_dict
+'''
 
 
-def get_user_matrix(data_dir):
-    bi_matrix = get_bipartite_matrix(data_dir)
-    # weighted
-    user_matrix = bi_matrix * bi_matrix.transpose()
-    return user_matrix
+class MultiPPR(object):
+    def __init__(self, damping_factors, matrix):
+        super(MultiPPR, self).__init__()
+        self.damping_factors = damping_factors
+        self.mat = matrix
 
-
-def get_pos_neg_df(csv_file, train_sid):
-    df = pd.read_csv(csv_file)
-    df['target'] = 1
-
-    user_negatives = []
-    for i in df['userId'].unique():
-        movie_array = df['movieId'][df['userId'] == i].to_numpy()
-        target_0 = list(set(train_sid) - set(movie_array))
-        user_negatives.append(np.array([np.repeat(i, len(target_0)), target_0]).T)
-    pd_list = []
-    for i in range(len(user_negatives)):
-        pd_list.append(pd.DataFrame(user_negatives[i], columns=['userId', 'movieId']))
-    n_df = pd.concat(pd_list)
-    n_df['target'] = 0
-    merged = pd.concat([df, n_df])
-    shuffled_pd = merged.groupby('userId').sample(frac=1)
-    return shuffled_pd
-
-
-def load_all(data_dir):
-    with open(os.path.join(data_dir, 'train_sid'), 'rb') as f:
-        train_sid = pickle.load(f)
-    n_items = len(train_sid)
-
-    train_df = get_pos_neg_df(os.path.join(data_dir, 'train.csv'), train_sid)
-    train_np = train_df.to_numpy()
-    vad_df = get_pos_neg_df(os.path.join(data_dir, 'vad.csv'), train_sid)
-    vad_np = vad_df.to_numpy()
-    test_df = get_pos_neg_df(os.path.join(data_dir, 'test.csv'), train_sid)
-    test_np = test_df.to_numpy()
-    return n_items, train_np, vad_np, test_np
-
-
-def make_user_neigh_dict(df):
-    user_item_dict = df.groupby('userId')['movieId'].apply(list).to_dict()
-    item_user_dict = df.groupby('movieId')['userId'].apply(list).to_dict()
-
-    per_user_neigh = defaultdict(list)
-    for user in list(user_item_dict.keys()):
-        item_seq = user_item_dict[user]
-        for item in item_seq:
-            user_seq = item_user_dict[item]
-            per_user_neigh[user].extend(user_seq)
-
-    per_user_sorted_neigh = defaultdict(list)
-    for user in list(per_user_neigh.keys()):
-        sorted_neigh = [neigh for neigh, cnt in Counter(per_user_neigh[user]).most_common()]
-        per_user_sorted_neigh[user] = sorted_neigh
-
-    del per_user_neigh
-    return per_user_sorted_neigh
+    def multi_context(self, target_idx):
+        base_zeros = np.zeros(self.mat.shape[0] - 1)
+        seed = np.insert(base_zeros, target_idx, 1)
+        multi_score = []
+        indices = []
+        for i in range(len(self.damping_factors)):
+            pagerank = PageRank(damping_factor=self.damping_factors[i])
+            ppr = pagerank.fit_transform(self.mat.toarray(), seeds=seed)
+            idx = np.argsort(ppr)[::-1]
+            sorted_scroes = ppr[idx]
+            multi_score.append(np.array(sorted_scroes))
+            indices.append(np.array(idx))
+        return np.array(multi_score), np.array(indices)
 
 
 if __name__ == '__main__':
-    # data_dir = './data/ml-1m'
-    # 정작 필요한 ml-20m 에서는 SIGKILL 로 쓰지도 못하고 ...
-    data_dir = './data/ml-20m'
+    data_dir = './data/ml-1m'
+    # data_dir = './data/ml-20m'
+    damping_factors = [0.30, 0.50, 0.75, 0.85, 0.95]
 
+    # item feature extraction
+    movie_mat = get_movie_matrix(data_dir)
+    print('movie_mat shape : ', movie_mat.shape)
+    multi_ppr = MultiPPR(damping_factors, movie_mat)
     start = time.time()
-    df = pd.read_csv(os.path.join(data_dir, 'train.csv'))
-    per_user_sorted_neigh = make_user_neigh_dict(df)
+    per_item_ppr_dict = {}
+    per_item_idx_dict = {}
+    for i in range(movie_mat.shape[0]):
+        scores, indices = multi_ppr.multi_context(i)
+        per_item_ppr_dict[i] = scores
+        per_item_idx_dict[i] = indices
+        if i % 100 == 0:  # for ml-1m
+        # if i % 10 == 0:  # for ml-20m
+            print('%d nodes processed!' % i)
+            print('upto now %f S passed' % (time.time() - start))
     end = time.time()
-    print('takes : ', end - start)
-    with open(os.path.join(data_dir, 'per_user_neigh.dict'), 'wb') as f:
-        pickle.dump(per_user_sorted_neigh, f)
+    print('multi-ppr processing takes : ', end - start)
+    with open(os.path.join(data_dir, 'per_item_ppr.dict'), 'wb') as f:
+        pickle.dump(per_item_ppr_dict, f)
+    with open(os.path.join(data_dir, 'per_item_idx.dict'), 'wb') as f:
+        pickle.dump(per_item_idx_dict, f)
 
-    print('len(per_user_sorted_neigh) : ', len(per_user_sorted_neigh))  # 5034
-    # df['userId'].unique()
-    # array([   0,    1,    2, ..., 6029, 6031, 6033])
-    # len(df['userId'].unique())
-    # 5034
-    # per_user_sorted_neigh 길이 확인 !
-    neigh_length = []
-    for i in list(per_user_sorted_neigh.keys()):
-        neigh_length.append(len(per_user_sorted_neigh[i]))
-    print('mean neighbor length : ', np.mean(neigh_length))
-    print('min neighbor length : ', np.min(neigh_length))
-    print('max neighbor length : ', np.max(neigh_length))
-
-    # 아래 상황은, per_user_sorted_neigh.keys() 를 사용하지 않고,
-    # range(len(per_user_sorted_neigh) 로 하다 발생한 문제인데,
-    # 근데, 왜 문제가 생겼지 ?! -_;
-    # 아, range() 를 쓰면, key 값이 0 부터 순차적으로 대입되는데,
-    # 사실 userId 가 중간에 filtering 으로 빠지는 부분도 존재하기 때문에
-    # 없는 userId 도 포함됨
-
-    # neighbor 수가 0 인 경우가 많음 ... 무려 966 명
-    # 근데, neighbor 가 0 이라는 건, 해당 user 의 item 이 하나도 없다는 건데,
-    # 이미 data filtering 에서 이거 걸렀을텐데 ?!?!
-    neigh_length_0 = []
-    for i, l in enumerate(neigh_length):
-        if l == 0:
-            neigh_length_0.append(i)
-    print('len(neigh_length_0) : ', len(neigh_length_0))
+    # user feature extraction
+    user_mat = get_user_matrix(data_dir)
+    print('user_mat shape : ', user_mat.shape)
+    multi_ppr = MultiPPR(damping_factors, user_mat)
+    start = time.time()
+    per_user_ppr_dict = {}
+    per_user_idx_dict = {}
+    for i in range(user_mat.shape[0]):
+        scores, indices = multi_ppr.multi_context(i)
+        per_user_ppr_dict[i] = scores
+        per_user_idx_dict[i] = indices
+        if i % 100 == 0:
+        # if i % 10 == 0:  # for ml-20m
+            print('%d nodes processed!' % i)
+            print('upto now %f S passed' % (time.time() - start))
+    end = time.time()
+    print('multi-ppr processing takes : ', end - start)
+    with open(os.path.join(data_dir, 'per_user_ppr.dict'), 'wb') as f:
+        pickle.dump(per_user_ppr_dict, f)
+    with open(os.path.join(data_dir, 'per_user_idx.dict'), 'wb') as f:
+        pickle.dump(per_user_idx_dict, f)
