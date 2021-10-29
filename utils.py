0dc71db995263c5564203c34fa0e2448a54eb109
sy-chung <38775109+sy-chung@users.noreply.github.com>
2021-10-29 12:23:58 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,64 +1,37 @@
+import torch
 import numpy as np
-import pandas as pd
-import pickle
-from scipy.sparse import csr_matrix
-import os
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 
 
-def get_bipartite_matrix(data_dir):
-    df = pd.read_csv(os.path.join(data_dir, 'filtered_pd.csv'))
-    # load filtered & idx mapped dataframe !
-    col = df['movieId']
-    row = df['userId']
-    data = np.ones(len(row), dtype=int)
-    print('bipartite mat : max(row) = ', max(row))
-    print('bipartite mat : max(col) = ', max(col))
-    print('unique # of row : ', len(pd.unique(row)))
-    print('unique # of col : ', len(pd.unique(col)))
-    bi_matrix = csr_matrix((data, (row, col)), shape=(max(row)+1, max(col)+1))
-    return bi_matrix
+def RECALL(predictions, targets, k):
+    topk_idx = torch.argsort(predictions, descending=True)[:k].detach().to('cpu').numpy()
+    target_ids = np.where(targets == 1)[0]
+    tmp = len(set(topk_idx) & set(target_ids))
+    dinorm = min(k, targets.sum())
+    return tmp / dinorm
+
+def NDCG(predictions, targets, k):
+    topk_idx = torch.argsort(predictions, descending=True)[:k].detach().to('cpu').numpy()
+    discount = 1. / np.log2(np.arange(2, k+2))
+    DCG = np.array(targets[topk_idx] * discount).sum()
+    IDCG = discount[:min(targets.sum(dtype=np.int32), k)].sum()
+    return DCG / IDCG
 
 
-def get_movie_matrix(data_dir):
-    bi_matrix = get_bipartite_matrix(data_dir)
-    movie_matrix = bi_matrix.transpose() * bi_matrix
-    return movie_matrix
-
-
-def get_user_matrix(data_dir):
-    bi_matrix = get_bipartite_matrix(data_dir)
-    user_matrix = bi_matrix * bi_matrix.transpose()
-    return user_matrix
-
-
-def get_pos_neg_df(csv_file, train_sid):
-    df = pd.read_csv(csv_file)
-    df['target'] = 1
-
-    user_negatives = []
-    for i in df['userId'].unique():
-        movie_array = df['movieId'][df['userId'] == i].to_numpy()
-        target_0 = list(set(train_sid) - set(movie_array))
-        user_negatives.append(np.array([np.repeat(i, len(target_0)), target_0]).T)
-    pd_list = []
-    for i in range(len(user_negatives)):
-        pd_list.append(pd.DataFrame(user_negatives[i], columns=['userId', 'movieId']))
-    n_df = pd.concat(pd_list)
-    n_df['target'] = 0
-    merged = pd.concat([df, n_df])
-    shuffled_pd = merged.groupby('userId').sample(frac=1)
-    return shuffled_pd
-
-
-def load_all(data_dir):
-    with open(os.path.join(data_dir, 'train_sid'), 'rb') as f:
-        train_sid = pickle.load(f)
-    n_items = len(train_sid)
-
-    train_df = get_pos_neg_df(os.path.join(data_dir, 'train.csv'), train_sid)
-    train_np = train_df.to_numpy()
-    vad_df = get_pos_neg_df(os.path.join(data_dir, 'vad.csv'), train_sid)
-    vad_np = vad_df.to_numpy()
-    test_df = get_pos_neg_df(os.path.join(data_dir, 'test.csv'), train_sid)
-    test_np = test_df.to_numpy()
-    return n_items, train_np, vad_np, test_np
+def result_plot(epochs, results, plot_label, y_label, save_name, title_label):
+    params = {'legend.fontsize': 'small',
+              'figure.figsize': (3.5, 2.5),
+              'axes.labelsize': 'x-small',
+              'axes.titlesize': 'x-small',
+              'xtick.labelsize': 'xx-small',
+              'ytick.labelsize': 'xx-small'}
+    plt.rcParams['figure.constrained_layout.use'] = True
+    pylab.rcParams.update(params)
+    plt.figure()
+    plt.plot(epochs, results, label=plot_label)
+    plt.xlabel('epochs')
+    plt.ylabel(y_label)
+    plt.title(title_label)
+    plt.savefig(save_name)
+    plt.show()
